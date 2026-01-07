7841c5341723e27bdb693ad2ffaa9bb226eedd92
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-07 20:25:27 -0300

    create product ok

--- src/Service/Catalog/Catalog.API/Products/CreateProductEndpoint.cs
+++ src/Service/Catalog/Catalog.API/Products/CreateProductEndpoint.cs
@@ -1,5 +1,28 @@
-﻿namespace Catalog.API.Products; 
+﻿using Carter;
+using Mapster;
+using MediatR;
 
-public class CreateProductEndpoint
+namespace Catalog.API.Products; 
+
+public record CreateProductRequest(string Name, List<string> Category, string Description,string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{result.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
 }
--- src/Service/Catalog/Catalog.API/Products/CreateProductHandler.cs
+++ src/Service/Catalog/Catalog.API/Products/CreateProductHandler.cs
@@ -1,9 +1,31 @@
-﻿namespace Catalog.API.Products; 
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products; 
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
 
-public record CreateProductResult(Guid Id); 
-
-public class CreateProductHandler
+internal class CreateProductCommandHandler(IDocumentSession session) 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new ProductModel
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
 }
